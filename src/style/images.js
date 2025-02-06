5309036f92d9f1a8217b8ebf38037835b2f11a5f
kuner19 <kunerxacierda@gmail.com>
2025-02-07 00:53:41 +0800

    add images

--- src/style/images.js
+++ src/style/images.js
@@ -4,6 +4,10 @@ import sunny from '../../assets/icons/sunny.png'
 import temp_white from '../../assets/icons/temp.png'
 import humid from '../../assets/icons/humidity.png'
 import wind from '../../assets/icons/wind.png'
+import night from '../../assets/icons/nighty.png'
+import snowy from '../../assets/icons/snowy.png'
+import thunderstorm from '../../assets/icons/thunderstorm.png'
+import rainy from '../../assets/icons/rainy.png'
 
 
 export default {
@@ -12,5 +16,9 @@ export default {
     sunny,
     temp_white,
     humid,
-    wind
+    wind,
+    night,
+    snowy,
+    thunderstorm,
+    rainy
 };
