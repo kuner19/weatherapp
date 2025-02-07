ec54052ee8aa4a118baed923e057d27d9d8ff62f
kuner19 <kunerxacierda@gmail.com>
2025-02-07 19:35:46 +0800

    check location startup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,24 @@
 import { Stack } from "expo-router";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import {useFonts} from 'expo-font';
 import { Text } from "react-native";
 import * as Splashscreen from 'expo-splash-screen';
 import "../global.css"
+import * as Location from 'expo-location'
 
 Splashscreen.preventAutoHideAsync()
 
 const Layout = () => {
+
+    const [location, setLocation] = useState<Location.LocationObject | null>(null);
+
     const [loaded,error] = useFonts({
         // Inter: require('../assets/fonts/Inter.ttf'),
         // Robot : require('../assets/fonts/Roboto-Light.ttf')
         'JosefinSlab': require('../assets/fonts/JosefinSlab-Regular.ttf')
     })
+
+
 
     // const onLayoutRootView = useEffect(async ()=>{
     //     if (loaded || error){
@@ -23,20 +29,41 @@ const Layout = () => {
     //     }
     //     Splashscreen.hideAsync()
     // },[loaded , error])
+   
+
+    useEffect(()=>{
+        async function getCurrentLocation() {
+      
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+              console.log('not granted')
+              return;
+            }
+      
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+          }
+  
+          
+        getCurrentLocation()
+    },[])
+
 
 
     const onLayoutRootView = useEffect(()=>{
-        if(loaded){
-            console.log(loaded)
-                    Splashscreen.hideAsync()
-        }  
-        Splashscreen.hideAsync()
+        if (location !== null){
+           // Splashscreen.hideAsync()
+        }
+       
         
-    },[loaded,error])
+    },[location])
+
+
+
     if (!loaded){
 
         console.log('not loaded')
-        Splashscreen.hideAsync()
+        // Splashscreen.hideAsync()
         return null
     }  
     return <Stack onLayout =  { onLayoutRootView } />
